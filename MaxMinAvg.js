bb2aaf87c07344010b35f8278332660954f6808a
xone <muftinmarmut@gmail.com>
2026-05-25 14:04:09 +0700

    docs: added JSDoc accumulator function

--- MaxMinAvg.js
+++ MaxMinAvg.js
@@ -1,3 +1,18 @@
+/**
+ * @param {Array<number>} num
+ * /
+/**
+ * @typedef {Object} hasil
+ * @property {number} max
+ * @property {number} min
+ * @property {number} avg 
+ */
+/**
+ * Menghitung nilai tertinggi, terrendah, dan rata-rata dari isi array number
+ * @param {Array<number>} num array contains numbers
+ * @returns {hasil} results of calculating the highest value, lowest value, and average of a set of numbers in an array
+ */
+
 export function Acumulator(num) {
   let max = 0;
   let min = 100;
